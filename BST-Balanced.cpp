075550f9187cce3ef107f94bdb7cd593738fb399
NGUYENUYEN0903 <44533077+NGUYENUYEN0903@users.noreply.github.com>
2019-04-27 21:16:52 +0200

    BST balanced (roi rac hoa dinh cua do thi)

---
+++ BST-Balanced.cpp
@@ -0,0 +1,22 @@
+class Indexer {         //Balanced BST (roi rac hoa cac dinh)
+private :
+    map<int,int> id;
+    vector<int> num;
+
+public :
+    int getId(int x) {              //return new ID of vertex
+        if (!id.count(x)) {
+            id[x] = num.size();
+            num.pb(x);
+        }
+        return id[x];
+    }
+
+    int getNum(int id) {            //return ancient ID of vertex
+        return num[id];
+    }
+
+    int size() {
+        return id.size();
+    }
+};
